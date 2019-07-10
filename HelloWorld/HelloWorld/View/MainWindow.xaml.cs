ad7190dfa60fa2f33f8f7772fecd6e8f58ad1dd1
Tobias Gärtner <tgaertner@recom.eu>
2019-07-10 13:51:08 +0200

    Initial Hello World

---
+++ HelloWorld/HelloWorld/View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace HelloWorld
+{
+    /// <summary>
+    ///     Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
