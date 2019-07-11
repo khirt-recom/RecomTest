285e594ca0e2c4c5d1c4151a7a9febde9812b1cd
Tobias Gärtner <tgaertner@recom.eu>
2019-07-11 15:55:50 +0200

    add view model tests
    Verknüpfte Arbeitselemente: #37

---
+++ HelloWorld/UnitTestProject/ViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HelloWorld.ViewModel;
+using NUnit.Framework;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class ViewModelTest
+    {
+        [Test]
+        public void ViewModel_TestGreeting()
+        {
+            MainViewModel vm = new MainViewModel();
+
+            Assert.IsNull(vm.Greeting);
+            Assert.NotNull(vm.GreetCommand);
+
+            vm.GreetCommand.Execute(null);
+
+            Assert.NotNull(vm.Greeting);
+            Assert.IsNotEmpty(vm.Greeting);
+            Assert.AreEqual("Hello Devs", vm.Greeting);
+            Assert.NotNull(vm.GreetCommand);
+        }
+    }
+}
